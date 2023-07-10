34cfd12489db89b0f80509975a6074e06370701e
Leonov Nikita <>
2023-07-10 13:35:55 +0300

    Отредактирована сцена

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HEXRPG.Core
 {
     public class Health : MonoBehaviour
     {
         [SerializeField] private float _maxHealth = 100f;
+        public Image fill;
+            
         bool isDead = false;
-
+        
         public bool IsDead()
         {
             return isDead;
@@ -15,10 +18,17 @@ namespace HEXRPG.Core
         public void TakeDamage(float damage)
         {
             _maxHealth = Mathf.Max(_maxHealth - damage, 0);
+            UpdateHealthBar();
             if (_maxHealth == 0)
             {
                 Death();
             }
+        }
+
+        private void UpdateHealthBar()
+        {
+            float hpBar = _maxHealth / 100;
+            fill.fillAmount = hpBar;
         }
 
         private void Death()
